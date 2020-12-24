dd6501af277e7f10aa6ac9a95f3e71e1fe35cf4e
Оля Кириченко <OlyaKir2001@mail.ru>
2020-12-25 02:43:36 +0700

    Плюсииик

--- Test/Program.cs
+++ Test/Program.cs
@@ -6,7 +6,13 @@ namespace Test
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Введите А:");
+            int a = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Введите B:");
+            int b = Int32.Parse(Console.ReadLine());
+
+            int c = a + b;
+            Console.WriteLine("=" + c);
         }
     }
 }
